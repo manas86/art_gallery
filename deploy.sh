c115d903ee6b9aa68b01c1a66b9a5b937c9a16f6
manas86 <manas.samantaray@gmail.com>
2019-09-27 11:37:57 +0200

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+aws deploy create-deployment \
+--application-name art_gallery \
+--deployment-config-name CodeDeployDefault.OneAtATime \
+--deployment-group-name art_gallery_deployment_group \
+--description "CodeDeploy Demo Deployment" \
+--github-location repository=linkedin-learning/ruby-demo-code,commitId=$(git rev-parse HEAD)
